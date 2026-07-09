ede79f79fc0ddbd6c6995a9fb29a281226d1b5dd
Muhammad Talha <muhammadtalhafd@gmail.com>
2026-07-09 08:29:18 +0600

    feat: Completed Navbar with Responsive

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+    const navBtn = document.getElementById('menu');
+
+    navBtn.addEventListener('click', function () {
+    const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+    navBtn.setAttribute('aria-expanded', !isExpanded);
+
+    document.getElementById('menubar').classList.toggle('hidden');
+    document.getElementById('menubar').classList.toggle('flex');
+})
